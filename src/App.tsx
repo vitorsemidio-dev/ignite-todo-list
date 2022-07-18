6e7b4dfb3b33ef9a8707bd4bec2e3a2e2c428c72
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-18 02:30:29 -0300

    feat: implemented create new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { NewTask } from "./Components/NewTask";
 import { Header } from "./Components/Header";
+import { useState } from "react";
+
+type Task = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
 
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function addNewTask(newTaskTitle: string) {
+    const id = new Date().getTime().toString();
+    const newTask: Task = { id, title: newTaskTitle, isCompleted: false };
+    setTasks((state) => [...state, newTask]);
+  }
+
   return (
     <div>
       <Header />
+      <NewTask onCreateNewTask={addNewTask} />
     </div>
   );
 }
