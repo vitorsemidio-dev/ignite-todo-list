378f3146ee5b34e39280cf3a3c758a0b8d414043
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-07-18 15:13:41 -0300

    feat: counting created and done tasks

--- src/Components/Tasks/index.tsx
+++ src/Components/Tasks/index.tsx
@@ -22,32 +22,58 @@ export function Tasks({ tasks, onCompleteTask, onRemoveTask }: TasksProps) {
     onRemoveTask(taskId);
   }
 
+  const totalTasksCreated = tasks.length;
+  const totalTasksDone = tasks.reduce(
+    (acc, cur) => acc + Number(cur.isCompleted),
+    0,
+  );
+
   return (
     <div className={styles.taskContainer}>
-      {tasks.map((task) => {
-        return (
-          <div key={task.id} className={styles.taskItemContainer}>
-            <input
-              className={styles.checkbox}
-              type="checkbox"
-              checked={task.isCompleted}
-              onChange={() => handleCheckChange(task.id)}
-            />
-            <p
-              className={
-                task.isCompleted ? styles.titleTaskCompleted : styles.titleTask
-              }>
-              {task.title}
-            </p>
-            <Trash
-              role="button"
-              className={styles.removeIcon}
-              size={20}
-              onClick={() => handleRemoveTask(task.id)}
-            />
-          </div>
-        );
-      })}
+      <div className={styles.taskInfoContainer}>
+        <p className={styles.infoTextCreated}>
+          Tarefas criadas{" "}
+          <span className={styles.taskInfoCounter}>{totalTasksCreated}</span>
+        </p>
+        <p className={styles.infoTextDone}>
+          Concluídas{" "}
+          <span className={styles.taskInfoCounter}>
+            <>
+              {totalTasksCreated === 0
+                ? totalTasksCreated
+                : `${totalTasksDone} de ${totalTasksCreated}`}
+            </>
+          </span>
+        </p>
+      </div>
+      <div className={styles.taskListContainer}>
+        {tasks.map((task) => {
+          return (
+            <div key={task.id} className={styles.taskItemContainer}>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                checked={task.isCompleted}
+                onChange={() => handleCheckChange(task.id)}
+              />
+              <p
+                className={
+                  task.isCompleted
+                    ? styles.titleTaskCompleted
+                    : styles.titleTask
+                }>
+                {task.title}
+              </p>
+              <Trash
+                role="button"
+                className={styles.removeIcon}
+                size={20}
+                onClick={() => handleRemoveTask(task.id)}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
